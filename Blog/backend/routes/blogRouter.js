e35cc98be02ce565bece40fab46d6bb6b6178319
Maryam fatima <112760984+maryam-fati@users.noreply.github.com>
2024-06-11 08:40:25 -0700

    Update blogRouter.js

--- Blog/backend/routes/blogRouter.js
+++ Blog/backend/routes/blogRouter.js
@@ -3,10 +3,10 @@ const { getBlog, getBlogById, saveBlog, deleteBlog, updateBlog } = require("../c
 
 const router = Router();
 
-router.get("/getBlogs", getBlog); // Change from POST to GET for fetching all blogs
-router.get("/getBlog/:id", getBlogById); // New route for fetching a single blog by ID
+router.get("/getBlogs", getBlog);
+router.get("/getBlog/:id", getBlogById);
 router.post("/save", saveBlog);
-router.put("/update/:id", updateBlog); // New route for updating a blog
+router.put("/update/:id", updateBlog);
 router.delete("/delete/:id", deleteBlog);
 
 module.exports = router;
